dea1315252b6e5b7e59544dac912837dc7f0d706
vivianxia01 <clairexxia@gmail.com>
2020-01-07 16:09:51 -0500

    initial project add

---
+++ src/test/java/vivian/springframework/springpetclinicapplication/SpringPetclinicApplicationTests.java
@@ -0,0 +1,14 @@
+package vivian.springframework.springpetclinicapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
